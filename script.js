2041f99ada68e7c9e73827659e913f68ffea5753
Isaac Young <isaac3.young6@gmail.com>
2026-01-22 09:56:27 -0800

    added more accessibility so arrows can control the rating

--- script.js
+++ script.js
@@ -1,25 +1,42 @@
-const rateBtns = document.querySelectorAll(".rating__button")
+const rateBtns = document.querySelectorAll(".rate__button")
 const submitBtn = document.querySelector("#submit-button")
 const userRating = document.querySelector("#user-rating")
 
-const clicksBtn = (btn) => {
-    rateBtns.forEach(button => {
-        button.classList.remove("clicked")
-        button.setAttribute("aria-pressed", "false")
-    })
-    btn.classList.add("clicked")
-    btn.setAttribute("aria-pressed", "true")
-    userRating.textContent = btn.value
-}
 
-rateBtns.forEach(btn => {
+rateBtns.forEach((btn, index) => {
+    btn.addEventListener("keydown", (e) => {
+        let newIndex;
+        if (e.key === "ArrowRight" || e.key === "ArrowDown") {
+            e.preventDefault();
+            newIndex = (index + 1) % rateBtns.length;
+        } else if (e.key === "ArrowLeft" || e.key === "ArrowUp") {
+            e.preventDefault();
+            newIndex = (index - 1 + rateBtns.length) % rateBtns.length;
+        }
+        if (newIndex !== undefined) {
+            
+            rateBtns.forEach(btn => btn.setAttribute("tabindex", "-1"));
+            rateBtns[newIndex].setAttribute("tabindex", "0");
+            rateBtns[newIndex].focus();
+
+            rateBtns.forEach(btn => btn.setAttribute("aria-checked", "false"));
+            rateBtns[newIndex].setAttribute("aria-checked", "true");
+        }
+    })
     btn.addEventListener("click", () => {
-        clicksBtn(btn)
+        rateBtns.forEach(b => {
+            b.setAttribute("aria-checked", "false")
+            b.setAttribute("tabindex", "-1")
+        })
+        btn.setAttribute("aria-checked", "true")
+        btn.setAttribute("tabindex", "0")
+        btn.focus()
+        userRating.textContent = btn.value
     })
 })
 
 submitBtn.addEventListener("click", () => {
-    if ([...rateBtns].some(btn => btn.classList.contains("clicked"))) {
+    if ([...rateBtns].some(btn => btn.getAttribute("aria-checked") === "true")) {
         document.querySelector(".rate").setAttribute("hidden", "hidden")
         document.querySelector(".thanks").removeAttribute("hidden")
     }
